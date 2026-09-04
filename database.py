647f23c1c6c3d91edd366e5fbe0bf64a661c1a49
Sahil Gautam <daushyant246@gmail.com>
2026-09-04 13:12:48 +0530

    Connect MarginMind to PostgreSQL

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./payments.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine=create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
-sessionlocal=sessionmaker(
+sessionlocal = sessionmaker(
     bind=engine,
     autoflush=False,
     autocommit=False
 )
-base=declarative_base()
+
+base = declarative_base()
